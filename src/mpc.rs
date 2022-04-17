e3efc4d9edf090aea5dc8b6f2da0d0c9bd2dc9b4
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-04-17 23:53:45 +0200

    Implement Garbler advance up to start

--- src/mpc.rs
+++ src/mpc.rs
@@ -3,20 +3,30 @@ use subtle::{Choice, ConditionallySelectable};
 
 use crate::circuit::Circuit;
 use crate::garbling::{garble, GarbledCircuit, WireKey, WireKeyPair};
-use crate::ot;
+use crate::ot::{self, OTError};
 
 /// An error that can happen while running our MPC protocol.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum MPCError {
     /// We've already finished the protocol when trying to advance.
     AlreadyFinished,
+    /// We received an unexpected message
+    UnexpectedMessage,
     /// An error occurring from the underlying oblivious transfer.
     OTError(ot::OTError),
+}
+
+impl From<ot::OTError> for MPCError {
+    fn from(e: ot::OTError) -> Self {
+        Self::OTError(e)
+    }
 }
 
 /// Represents some kind of message that can be sent during the MPC protocol.
 #[derive(Clone, Debug)]
 pub enum Message {
+    /// The first message to initiate the garbler.
+    Start,
     /// We're sending messages associated with all of our OT instances.
     OTMessages(Vec<ot::Message>),
     /// The garbler sends over the circuit to evaluate.
@@ -77,6 +87,39 @@ impl Garbler {
             garbled,
             senders,
         }
+    }
+
+    pub fn advance<R: RngCore + CryptoRng>(
+        &mut self,
+        rng: &mut R,
+        message: Message,
+    ) -> Result<MPCOutput, MPCError> {
+        let (new_self, res) = match std::mem::replace(self, Self::Done) {
+            Self::ObliviousTransfer {
+                a_keys,
+                garbled,
+                mut senders,
+            } => match message {
+                Message::Start => {
+                    let messages_result: Result<Vec<_>, OTError> = senders
+                        .iter_mut()
+                        .map(|sender| match sender.advance(rng, ot::Message::Start)? {
+                            ot::OTOutput::Message(m) => Ok(m),
+                            _ => panic!("OT sending start didn't produce a message"),
+                        })
+                        .collect();
+                    let messages = messages_result?;
+                    (
+                        Self::ObliviousTransfer { a_keys, garbled, senders },
+                        MPCOutput::Message(Message::OTMessages(messages))
+                    )
+                }
+                _ => return Err(MPCError::UnexpectedMessage),
+            },
+            _ => unimplemented!(),
+        };
+        *self = new_self;
+        Ok(res)
     }
 }
 
