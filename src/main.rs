78aacd881b82033c6900d0d464bf9cf374b17bb9
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-04-18 18:22:55 +0200

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,24 @@ mod circuit;
 mod garbling;
 mod mpc;
 mod ot;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Args {
+    /// If true, then this will be the garbler listening for connections
+    #[structopt(short, long)]
+    listen: bool,
+    /// The path to the circuit file
+    #[structopt(short, long, parse(from_os_str))]
+    circuit: PathBuf,
+    /// The network address to listen on, or to connect to
+    #[structopt(name = "ADDRESS")]
+    address: String
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    println!("{:?}", args);
 }
